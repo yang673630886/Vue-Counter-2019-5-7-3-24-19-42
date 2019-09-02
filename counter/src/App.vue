4679849d22d6aba1e809ba52d9a11a558e2e5ef2
yangjingsheng <673630886@qq.com>
2019-09-02 21:27:20 +0800

    tijiao

---
+++ counter/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <input v-model="num" @input="initData" />
+    <Test v-for=" n in parseInt(num)" v-bind:key="n" v-bind:index="n" v-on:update="sumAll"></Test>
+    <p>{{sumInit}}</p>
+  </div>
+</template>
+<script>
+import Test from "./components/Test.vue";
+
+export default {
+  name: "app",
+  components: {
+    // HelloWorld,
+    Test
+  },
+  data() {
+    return {
+      num: 0,
+      sumInit: 0
+    };
+  },
+
+  methods: {
+    sumAll: function(a) {
+      this.sumInit += a;
+    },
+    initData: function() {
+      this.sumInit = ((1 + parseInt(this.num)) * parseInt(this.num)) / 2;
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ counter/src/components/Test.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <span>{{index}}:</span>
+    <button v-on:click="add">+</button>
+    <span>{{number1}}</span>
+    <button v-on:click="minus">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    index: Number,
+    str: String
+  },
+  data() {
+    return {
+      number1: this.index
+    };
+  },
+  watch:{
+      num:{
+         init()
+      }
+  },
+  methods: {
+    add: function() {
+      this.number1++;
+      this.$emit("update", 1);
+    },
+    minus: function() {
+      this.number1--;
+      this.$emit("update", -1);
+    },
+    init: function(str) {
+      window.console.log("aaa");
+      this.str = str;
+      this.number1 = this.index;
+    }
+  }
+};
+</script>
